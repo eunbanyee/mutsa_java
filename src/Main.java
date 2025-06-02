57bcbc04ec96dca5684691ce71da90beff1ab581
eunbanyee <sd2d60@gmail.com>
2025-06-02 11:05:57 +0900

    연산자(operator)

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,30 @@ public class Main {
        int quotient = a / b;
        int remainder = a % b;
 
+       // 사칙 연산자
        System.out.println(sum);
        System.out.println(difference);
        System.out.println(product);
        System.out.println(quotient);
        System.out.println(remainder);
-        }
+
+       //복합 대입 연산자
+        a += 5; // a = a + 5;
+        System.out.println(a);
+
+        b += 1;
+        System.out.println(b);
+
+        b *= 4;
+        System.out.println(b);
+
+        b /= 3;
+        System.out.println(b);
+
+        b %= 2;
+        System.out.println(b);
+    // 증감 연산자
+        a ++ ;
+        System.out.println(a);
+    }
     }
