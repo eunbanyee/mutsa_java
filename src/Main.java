deb1a6335d44d45e1a9636513b1fe2b383b76bbb
eunbanyee <sd2d60@gmail.com>
2025-06-02 10:29:42 +0900

    연산자(operator)

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,15 @@ public class Main {
        int a = 10, b = 3;
 
        int sum = a + b;
+       int difference = a - b;
+       int product = a * b;
+       int quotient = a / b;
+       int remainder = a % b;
 
-        System.out.println(sum);
-
-        int difference = a-b;
-        int product = a*b;
-        int quotient = a/b;
-        int remainer = a%b;
-
-        System.out.println(difference);
-        System.out.println(product);
-        System.out.println(quotient);
-        System.out.println(remainer);
+       System.out.println(sum);
+       System.out.println(difference);
+       System.out.println(product);
+       System.out.println(quotient);
+       System.out.println(remainder);
         }
     }
